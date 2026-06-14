536a20954108f9dc19f128cfb6d95ac293b28680
simon colohan <simon.colohan+github@gmail.com>
2026-06-14 11:49:11 +0100

    Add a child

---
+++ frontend/src/SensorSocket.vue
@@ -0,0 +1,9 @@
+<script setup>
+const emit = defineEmits(['response'])
+
+emit('response', 'hello from child')
+</script>
+
+<template>
+  <h2>Need to open a websocket and emit the data back up to the app</h2>
+</template>
