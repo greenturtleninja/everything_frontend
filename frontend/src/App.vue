e4e5cb1f9d662582c1943db6397d7da139724028
simon colohan <simon.colohan+github@gmail.com>
2026-06-15 15:50:50 +0100

    add more stuff to frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,56 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import SensorSocket from './SensorSocket.vue';
-
-const childMsg = ref('No child msg yet')
-
-function rectangle() {
-  const canvas = <HTMLCanvasElement> document.getElementById("sensor");
-  if (canvas) {
-    const ctx = canvas.getContext("2d");
-    if (ctx) {
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
-      ctx.fillStyle = "green";
-      ctx.fillRect(10, 10, 150, 100);
-    }
-  }
-}
-
-function showCanvas() {
-  const canvas = <HTMLCanvasElement> document.getElementById("sensor");
-  if (canvas) {
-    const ctx = canvas.getContext("2d");
-    if (ctx) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-        ctx.beginPath();
-        ctx.moveTo(60, 40);
-        ctx.lineTo(180, 90);
-        ctx.lineTo(130, 210);
-        ctx.lineTo(10, 160);
-        ctx.closePath();
-        ctx.fillStyle = "blue";
-        ctx.fill();
-        ctx.strokeStyle = "lightgray";
-      for (let i = 0; i < 16; i++) {
-        ctx.moveTo(i * 20, 0);
-        ctx.lineTo(i * 20, 300);
-        ctx.moveTo(0, i * 20);
-        ctx.lineTo(300, i * 20);
-        ctx.stroke();
-      }
-    }
-  }
-}
 </script>
 
 <template>
-  <h1>Sensor Output</h1>
-  <SensorSocket @response="(msg) => childMsg = msg"/>
-  {{ childMsg }}
-  <button @click="rectangle">Sensor</button>
-  <button @click="showCanvas">Test Canvas</button>
-  <br />
-  <canvas id="sensor" width="1500" height="1500" style="border:1px solid #000000;"></canvas>
+  <h1>Test Stuff</h1>
+  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
+  <nav>
+    <RouterLink to="/">Home</RouterLink> --
+    <RouterLink to="/sensor">Sensor</RouterLink> --
+    <RouterLink to="/eagle">Eagle Bank</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped></style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomeView from '../views/Home.vue'
+import SensorView from '../views/Sensor.vue'
+import EagleView from '../views/EagleBank.vue'
+
+const routes = [
+  { path: '/', component: HomeView },
+  { path: '/sensor', component: SensorView },
+  { path: '/eagle', component: EagleView },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
