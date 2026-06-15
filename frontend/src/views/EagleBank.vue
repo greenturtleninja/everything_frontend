e60c2e5e91e46af397ea91f87ac63119089b23dd
simon colohan <simon.colohan+github@gmail.com>
2026-06-15 15:51:12 +0100

    Add more apps to play with

---
+++ frontend/src/views/EagleBank.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+<template>
+    <div>
+        <h2>Login</h2>
+    </div>
+    <div>
+        <h2>Accounts</h2>
+    </div>
+    <div>
+        <h2>User</h2>
+    </div>
+</template>
