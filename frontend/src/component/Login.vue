0fadfb0ac389a325f29ff3af569035684e0ce736
simon colohan <simon.colohan+github@gmail.com>
2026-06-17 15:24:55 +0100

    start to add the login for frontend

---
+++ frontend/src/component/Login.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const username = ref('Username');
+const password = ref ('##########');
+
+const loginUrl: string = 'https://localhost:8080/login';
+
+type LoginUser = {
+    username: string;
+    password: string;
+    token?: string;
+} 
+
+async function login() {
+    const loginUser: LoginUser = {
+        username: username.value,
+        password: password.value,
+    };
+
+    const token: Response = await fetch(loginUrl, {
+        method: 'POST',
+        body: JSON.stringify(loginUser),
+    });
+}
+
+</script>
+<template>
+    <div>
+        <h3>Password:</h3>
+        <input v-model="username" placeholder="Username">
+    </div>
+    <div>
+        <h3>Password:</h3>
+        <input v-model="password" placeholder="############">
+    </div>
+    <div>
+        <button @click="login" >Login</button>
+    </div>
+</template>
--- frontend/src/views/EagleBank.vue
+++ frontend/src/views/EagleBank.vue
@@ -1,13 +1,28 @@
-<script lang="ts">
+<script setup lang="ts">
+import { ref } from 'vue';
+import Login from '../component/Login.vue';
+import Accounts from '../component/Accounts.vue';
+import User from '../component/User.vue';
+
+
+const displayItem = ref('hello');
+
+function showComponent(component: string) {
+    displayItem.value = component;
+}
+
 </script>
 <template>
     <div>
-        <h2>Login</h2>
+        <h2 @click="showComponent('login')">Login</h2>
+        <Login v-if="displayItem=='login'"></Login>
     </div>
     <div>
-        <h2>Accounts</h2>
+        <h2 @click="showComponent('accounts')">Accounts</h2>
+        <Accounts v-if="displayItem=='accounts'"></Accounts>
     </div>
     <div>
-        <h2>User</h2>
+        <h2 @click="showComponent('user')">User</h2>
+        <User v-if="displayItem=='user'"></User>
     </div>
 </template>
